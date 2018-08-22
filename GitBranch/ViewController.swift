c50654f20a89c6023c78e9ea6508aae5b55fab15
Sanket Ray <iamsanketray123@gmail.com>
2018-08-22 12:06:56 +0530

    Added constants

--- GitBranch/ViewController.swift
+++ GitBranch/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    let jeet = "Jeet"
+    let sanket = "Sanket"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
